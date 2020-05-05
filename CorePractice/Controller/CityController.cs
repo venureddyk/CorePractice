5f7a8619bd2177ee7b54aea5b2448ca99ed00927
VENU K <venureddyk03@hotmail.com>
2020-05-05 15:26:49 -0400

    Created new Asp.net core application

---
+++ CorePractice/Controller/CityController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CorePractice.Controller
+{
+    [ApiController]
+    public class CityController:ControllerBase
+    {
+        [HttpGet("api/city")]
+        public JsonResult GetCity()
+        {
+
+            var ss = new List<CityModel>
+            {
+                new CityModel
+                {
+                    Id=1,
+                    Name="Hyderabad",
+                    Description="Great City"
+                },
+                new CityModel
+                {
+                    Id=2,
+                    Name="Banglore",
+                    Description="Good City"
+                }
+            };
+            return new JsonResult(ss);
+        }
+   }
+}
